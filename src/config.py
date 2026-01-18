63f52a8a4f68c854ad209abcca6a59017260fe23
agentdot <b.mallieck@gmail.com>
2026-01-18 07:34:29 +0000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -14,4 +14,5 @@ EMBED_MODEL = "text-embedding-3-small"
 KMEANS_K = 80
 
 DRIVE_EMB_STORE = "embeddings_store.parquet"
+DRIVE_DISCOVERY_QUEUE = "discovery_queue.parquet"
 DRIVE_CENTROIDS = "kmeans_centroids.parquet"
